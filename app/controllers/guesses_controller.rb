e6587d37e9428f556f98932db31be47342ccc00a
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-10-25 20:09:09 -0500

    Map Form to List

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,10 +1,24 @@
 class GuessesController < ApplicationController
   def index
+    #params = {"first_number"=> " ", "second_number" =>"", "third_number"=> ""}
+
+    @first_number = params["first_number"]
+    @second_number = params["second_number"]
+    @third_number = params["third_number"]
+
+    if @first_number < @second_number && @second_number < @third_number
+      @test = "yes!"
+    else
+      @test = "no."
+    end
 
     render("all_guesses.html.erb")
   end
 
   def answer
+    # params = {"rule" => " "}
+
+    @user_answer = params["rule"]
 
     render("show_answer.html.erb")
   end
