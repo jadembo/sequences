bdfced273fc13c86548365a7f9060cab9179c55f
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-10-27 13:50:06 -0500

    Final

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -13,19 +13,19 @@ class GuessesController < ApplicationController
     end
 
     @list = Guess.all
-    
+
     render("all_guesses.html.erb")
   end
 
   def index
-    g=Guess.new
-    g.first_num = params["first_number"]
-    g.second_num = params["second_number"]
-    g.third_num = params["third_number"]
-    g.save
-    @list = Guess.all
 
-    render("all_guesses.html.erb")
+      g=Guess.new
+      g.first_num = params["first_number"].to_i
+      g.second_num = params["second_number"].to_i
+      g.third_num = params["third_number"].to_i
+      g.save
+      @list = Guess.all
+        render("all_guesses.html.erb")
   end
 
   def answer
